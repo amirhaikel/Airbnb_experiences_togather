d61bad1fb67b109b40d4583e5bd078eff86a8f7a
YongChian Teo <yong-chian@hotmail.com>
2023-03-02 02:32:43 +0800

    seed file in progress

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,9 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+require "open-uri"
+
+file = URI.open("some_picture_link")
+experience = Experience.new(name: "XXX", description: "XXXXXX", price: XXXXXX)
+experience.photos.attach(io: file, filename: "xxx.png", content_type: "image/png")
+experience.save
