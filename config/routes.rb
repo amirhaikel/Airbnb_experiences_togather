a6beea407b7ad3de7f37524906068493ed9ca141
YongChian Teo <yong-chian@hotmail.com>
2023-03-04 16:55:47 +0800

    booking model in progress

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,13 @@
 Rails.application.routes.draw do
-  get 'reviews/new'
   devise_for :users
   root to: "pages#home"
   get "/about", to: "pages#about"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
+
   # Defines the root path route ("/")
   resources :experiences do
-    resources :reviews, only: [:new]
+    resources :bookings, only: [:new, :create, :show, :index]
   end
   # root "articles#index"
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_03_04_032458) do
+ActiveRecord::Schema[7.0].define(version: 2023_03_04_070315) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -43,9 +43,9 @@ ActiveRecord::Schema[7.0].define(version: 2023_03_04_032458) do
   end
 
   create_table "bookings", force: :cascade do |t|
-    t.bigint "user_id", null: false
-    t.bigint "experience_id", null: false
     t.date "booking_date"
+    t.bigint "experience_id", null: false
+    t.bigint "user_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["experience_id"], name: "index_bookings_on_experience_id"
